6ba3fa191c25cf906f1a08c458b0c3d51dde3ed3
Zhan Li <zhanli1986@gmail.com>
2016-02-22 17:37:18 -0500

    set unicode font explicitly

--- .emacs.d/config/my-config.el
+++ .emacs.d/config/my-config.el
@@ -54,7 +54,16 @@
 (global-whitespace-mode t)
 (dynamic-completion-mode t)
 (icomplete-mode 1)
-(subword-mode 1)
+;; (subword-mode 1)
+
+;;;;;;;;;;;;;;;;;;;;
+;;; set up unicode
+;;(prefer-coding-system       'utf-8)
+;;(set-default-coding-systems 'utf-8)
+(set-terminal-coding-system 'utf-8)
+;;(set-keyboard-coding-system 'utf-8)
+;;(setq default-buffer-file-coding-system 'utf-8)                      
+;;(setq x-select-request-type '(UTF8_STRING COMPOUND_TEXT TEXT STRING))
 
 
 (provide 'my-config)
