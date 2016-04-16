9aab2ea6b75c4ead2ec3c77e46315c2841a8d37e
Zhan Li <zhanli1986@gmail.com>
2016-04-16 10:34:38 -0400

    check how many emacs clients and frames are running before closing emacs daemon server when c-x c-c

--- .emacs.d/config/my-keys.el
+++ .emacs.d/config/my-keys.el
@@ -23,5 +23,38 @@
 ;; Completion
 (global-set-key (kbd "M-/") 'hippie-expand)
 
+;; check if this is the last emacs daemon client. If so, close emacs
+;; daemon server
+(defun kill-emacs-sever ()
+  (interactive)
+  (setq active-clients-or-frames ( or (> (length server-clients) 1)
+                                      (> (length (frame-list)) 2)
+                                             ))
+  ; When displaying the number of clients and frames: 
+  ; subtract 1 from the clients for this client.
+  ; subtract 1 from the frames for this frame
+  (when ( or (not active-clients-or-frames)
+                    (yes-or-no-p (format "There are currently the other %d clients and %d frames. Exit anyway?" (- (length server-clients) 1) (- (length (frame-list)) 2))))
+    ; If the user quits during the save dialog then don't exit emacs.
+    ; Still close the terminal though.
+    (let((inhibit-quit t))
+      ; Save buffers
+      (with-local-quit
+          (save-some-buffers)) 
+
+      (if quit-flag    
+          (setq quit-flag nil)  
+          ; Kill all remaining clients
+          (progn
+            (dolist (client server-clients)
+              (server-delete-client client))
+            ; Exit emacs
+            (kill-emacs))
+      )
+    )
+  )
+)
+(global-set-key (kbd "C-x C-c") 'kill-emacs-sever)
+
 (provide 'my-keys)
 ;;; my-keys.el ends here
