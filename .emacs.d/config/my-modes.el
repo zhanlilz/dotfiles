bf5b4e96198cada20a09fb5465cdd87d9d8e0562
Zhan Li <zhanli1986@gmail.com>
2016-10-17 17:53:04 -0400

    fix loading matlab-mode

--- .emacs.d/config/my-modes.el
+++ .emacs.d/config/my-modes.el
@@ -1,13 +1,7 @@
 ;;; ... my-modes.el --- customize the language mode emacs dissplays ...
 
 ;;; ... matlab mode ...
-(autoload 'matlab-mode "matlab" "Matlab Editing Mode" t)
-(add-to-list
-  'auto-mode-alist
-  '("\\.m$" . matlab-mode))
-(setq matlab-indent-function t)
-(setq matlab-shell-command "matlab")
-(setq matlab-auto-fill nil)
+(require 'matlab-load)
 (custom-set-variables
  '(matlab-shell-command-switches '("-nodesktop -nosplash")))
 ;;; ................... 
